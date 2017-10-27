28d6838893680a20b28f01402096ded4158fa20b
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-10-27 17:48:06 +0300

    Task 1, 3, 4 added

---
+++ HW9_server/script/main.js
@@ -0,0 +1,48 @@
+/* 1. Используя пример со слайдов “Node.js: server - read from file system”, модифицировать ответ так, чтобы 
+сервер мог отдавать любые картинки, а не только картинку с именем "img.png".
+*/
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+
+    if (/jpg|png|gif|jpeg/.test(req.url)) {
+        fs.readFile(req.url.replace('/', ''), (err, data) => {
+            if (err) {
+                res.end('<strong>Error</strong>');
+                throw new Error('Error while reading img');
+            };
+            res.end(data);
+        });
+        return;
+    }
+    fs.readFile('../index.html', 'utf-8', (err, data) => {
+        if (err) {
+            res.end('<strong>Error</strong>');
+            throw new Error('Error while reading index.html');
+        };
+        const getDate = () => new Date().toLocaleDateString();
+        res.end(data.replace('</body>', `${getDate()} <script src="script/index.js"></script></body>`));
+    })
+
+    // res.setHeader('Content-Type', 'script/index.js')
+
+    // fs.readFile('index.js', (err, data) => {
+    //     if (err) {
+    //         res.end('<strong>Error</strong>');
+    //         throw new Error('Error while reading img');
+    //     };
+    //     res.end(data => console.log(data));
+    // });
+
+});
+server.listen(3000, () => console.log('Listen on localhost 3000'));
+
+/* 2. Используя решение из предыдущей задачи, подключить к index.html файл main.js. Скрипт должен выводить в 
+консоль браузера размеры картинки.
+Подсказка: сервер должен уметь “отдавать” js-файлы.
+*/
+
+/* 3. Создать package.json и сделать в нём изменения, чтобы сервер мог быть запущен из консоли командой npm run server.
+ */
+/* 4. Модифицировать ответ сервера так, чтобы внизу страницы всегда отображалась текущая дата (число, месяц, год). */
