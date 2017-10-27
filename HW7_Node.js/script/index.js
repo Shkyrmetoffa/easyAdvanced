f80446d287764bc270d8708c5324b85657936c87
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-10-27 17:54:39 +0300

    Task 1, 3, 4 added

--- HW7_Node.js/script/index.js
+++ HW7_Node.js/script/index.js
@@ -1,4 +1,35 @@
 const http = require('http');
-const server = http.createServer((req, res) => res.end('Yahoo!'));
+const fs = require('fs');
+let counter = 0;
 
-server.listen(3000);
+const server = http.createServer((req, res) => {
+    fs.readFile('../my-img.jpg', (err, data) => {
+            if (err) {
+                res.end('<strong>Error</strong>');
+                throw new Error('Error while reading index.html');
+            };
+            // res.setHeader('Content-Type', 'image/jpg'); 
+            res.end(data);
+        })
+        //
+        // console.log(req.headers, req.method, req.url);
+        // res.setHeader('Content-Type', 'text/html');
+        // http.get('http://wttr.in/~kharkov', (response) => {
+        //         if (response.statusCode !== 200) {
+        //             res.end('<strong style="color:red">Error!</stong>');
+        //             return;
+        //         }
+        //         response.setEncoding('utf-8');
+        //         let result = "";
+        //         response.on('data', data => result += data);
+        //         response.on('end', () => {
+        //             const upgradeResult = result.replace('</style>', 'body{background: red}</style>');
+        //             res.end(upgradeResult);
+        //         })
+
+    //     })
+    // res.end('<strong>Hello, Node!</stong>');
+});
+
+
+server.listen(3000, () => console.log('Server is listening on 3000'));
