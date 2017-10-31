2c086e2da00d082fe679859996b949481ceebfd4
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-10-31 22:09:41 +0200

    append fixed

--- HW10_webpack/js/src/app.js
+++ HW10_webpack/js/src/app.js
@@ -4,6 +4,4 @@ const main = require('./scripts/main')();
 const $ = require('jquery');
 const body = $('body');
 
-body.append(header);
-body.append(main);
-body.append(footer);
+body.append(header).append(main).append(footer);
