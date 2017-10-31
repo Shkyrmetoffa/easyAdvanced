8a2cc223c9842773bfb82bd79644da667c449de6
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-10-31 22:04:04 +0200

    bundle.js and dist deleted

--- HW10_webpack/js/src/app.js
+++ HW10_webpack/js/src/app.js
@@ -2,7 +2,8 @@ const header = require('./scripts/header')();
 const footer = require('./scripts/footer')();
 const main = require('./scripts/main')();
 const $ = require('jquery');
+const body = $('body');
 
-$('body').append(header);
-$('body').append(main);
-$('body').append(footer);
+body.append(header);
+body.append(main);
+body.append(footer);
--- HW10_webpack/js/src/scripts/elementBuilder.js
+++ HW10_webpack/js/src/scripts/elementBuilder.js
@@ -1,7 +1,8 @@
 const $ = require('jquery');
 module.exports = (tag = 'div', content = '', className = 'box') => {
     const element = document.createElement(tag);
-    $(element).addClass(className)
+    $(element)
+        .addClass(className)
         .html(content);
     return element;
 }
